90988a68893e9484d3bfabfd1c801535167d2dbe
kevin <kevinmarqueskmp@gmail.com>
2021-09-03 11:10:33 -0300

    Carousel is responsive now

--- Scripts/BlogCarousel.js
+++ Scripts/BlogCarousel.js
@@ -1,7 +1,25 @@
 $(document).ready(() => {
 	$('.owl-carousel').owlCarousel({
 		loop: true,
+
 		autoplay: true,
-		autoplayTimeout: 5000
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+
+		responsive: {
+			0: {
+				items: 1
+			},
+
+			600: {
+				items: 2
+			},
+
+			1000: {
+				items: 3
+			}
+		}
 	});
 });
+
+
